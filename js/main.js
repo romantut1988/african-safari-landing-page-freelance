a8d76179e79523ff2565a8c0e3c77f117f59e82b
Roman <romantut1988@mail.ru>
2021-11-20 19:38:56 +0300

    styles for the slider in the section gallery

--- js/main.js
+++ js/main.js
@@ -8,5 +8,20 @@ $(function(){
         fixedContentPos: false
     });
 
-    $('.gallery__slider').slick();
+    $('.gallery__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+    });
+
+    $('.gallery__item-inner').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+    });
 });
